f9901a0f07f285e8d73aef986d6505a0f866957d
manto21 <manojkumare2012@gmail.com>
2023-09-12 20:43:46 +0100

    Unit test cases covered

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # DLG-angular
+
+# Used Direct Line Group's logo on the Homepage as a sample.
+
+# Used Font Awesome to use Nunito Sans font.
+
+# Given dummy Login links at the bottom of the page and also on Success banner just to show the look and feel of a Signup page.
+
+# On submitting the form with fully completed form, I have used Mocky API to get a mock response.
--- src/app/sign-up/sign-up.component.spec.ts
+++ src/app/sign-up/sign-up.component.spec.ts
@@ -2,16 +2,20 @@ import { HttpClient, HttpHandler } from "@angular/common/http";
 import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { SignUpComponent } from "./sign-up.component";
+import { DataApiService } from "../app-data/data-api.service";
 
 describe("SignUpComponent", () => {
     let component: SignUpComponent;
     let fixture: ComponentFixture<SignUpComponent>;
-    
+    let dataApiServiceSpy: any;
+
     beforeEach(waitForAsync(() => {
+        dataApiServiceSpy = jasmine.createSpyObj('DataApiService', ['userSignUp']);
         TestBed.configureTestingModule({
             declarations: [SignUpComponent],
-            imports: [HttpClientTestingModule], 
-            providers: [HttpClient, HttpHandler ]
+            imports: [HttpClientTestingModule],
+            providers: [HttpClient, HttpHandler,
+                { provide: DataApiService, dataApiServiceSpy }]
         }).compileComponents();
     }));
 
@@ -23,5 +27,22 @@ describe("SignUpComponent", () => {
 
     it("should create", () => {
         expect(component).toBeTruthy();
+    });
+
+    it("should call buildForm method", () => {
+        component.buildForm();
+        expect(dataApiServiceSpy.userSignUp).toHaveBeenCalled();
+        expect(component.enableSubmitBtn).not.toBeNull();
+    });
+
+    it("should call submit method", () => {
+        component.submit();
+        expect(dataApiServiceSpy.userSignUp).toHaveBeenCalled();
+        expect(component.signUpSuccess).toBeTrue();
+    });
+    
+    it("should call closeBanner method", () => {
+        component.closeBanner();
+        expect(component.signUpSuccess).toBeFalse();
     });
 });
